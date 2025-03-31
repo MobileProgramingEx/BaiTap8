b5e5796b07ebc5ffca207d6b4f4470d9f4a373e2
huypham <huyphuocbinh204@gmail.com>
2025-03-31 11:22:46 +0700

    first commit

---
+++ ViewFlipper_CricleIndicator/app/src/main/java/baitap/example/viewflipper_cricleindicator/SlideShowActivity.java
@@ -0,0 +1,34 @@
+package baitap.example.viewflipper_cricleindicator;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlideShowActivity extends AppCompatActivity {
+
+    private SliderView sliderView;
+    private List<ImageModel> images;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_show);
+
+        sliderView = findViewById(R.id.imageSlider);
+
+        images = new ArrayList<>();
+        images.add(new ImageModel(R.drawable.image1));
+        images.add(new ImageModel(R.drawable.image2));
+
+        SliderAdapter adapter = new SliderAdapter(images);
+        sliderView.setSliderAdapter(adapter);
+        sliderView.setAutoCycle(true);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderView.setScrollTimeInSec(3);
+        sliderView.startAutoCycle();
+    }
+}
---
+++ ViewFlipper_CricleIndicator/app/src/main/java/baitap/example/viewflipper_cricleindicator/ViewPager2Activity.java
@@ -0,0 +1,33 @@
+package baitap.example.viewflipper_cricleindicator;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import java.util.ArrayList;
+import java.util.List;
+import me.relex.circleindicator.CircleIndicator3;
+
+public class ViewPager2Activity extends AppCompatActivity {
+    private ViewPager2 viewPager2;
+    private CircleIndicator3 circleIndicator;
+    private List<ImageModel> images;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager2);
+
+        viewPager2 = findViewById(R.id.viewPager2);
+        circleIndicator = findViewById(R.id.circle_indicator);
+
+        images = new ArrayList<>();
+        images.add(new ImageModel(R.drawable.image1));
+        images.add(new ImageModel(R.drawable.image2));
+
+        ImageAdapter adapter = new ImageAdapter(images);
+        viewPager2.setAdapter(adapter);
+
+        circleIndicator.setViewPager(viewPager2);
+    }
+}
